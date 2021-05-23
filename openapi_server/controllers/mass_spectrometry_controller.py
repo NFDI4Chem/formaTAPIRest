b3a4bd208ef9a648afa4400160da54b098fbc833
Steffen Neumann <sneumann@ipb-halle.de>
2021-05-23 12:50:20 +0200

    First working mzXML2mzML

--- openapi_server/controllers/mass_spectrometry_controller.py
+++ openapi_server/controllers/mass_spectrometry_controller.py
@@ -1,5 +1,6 @@
 import connexion
 import six
+import subprocess
 
 from openapi_server import util
 
@@ -17,9 +18,12 @@ def convert_pwi_zmz_data2mz_ml(body=None):  # noqa: E501
     with open('/tmp/input.mzData', 'wb') as infile:
         infile.write(body)
 
-    subprocess.call(["/usr/bin/msconvert", "/tmp/input.mzData", "/tmp/output.mzML"], shell=False)
+    subprocess.call(["/usr/bin/msconvert", \
+                    "/tmp/input.mzData", \
+                    "--outdir", "/tmp", \
+                    "--outfile", "output.mzML"], \
+                    shell=False)
 
     with open('/tmp/output.mzML', 'rb') as outfile:
         data = outfile.read()
-
     return data
