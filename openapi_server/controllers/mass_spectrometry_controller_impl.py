487b7b3982226a6235be4b3729c8ed8a46abe62d
Steffen Neumann <sneumann@ipb-halle.de>
2021-05-29 22:19:15 +0200

    Use K8S compatible temporary names

--- openapi_server/controllers/mass_spectrometry_controller_impl.py
+++ openapi_server/controllers/mass_spectrometry_controller_impl.py
@@ -17,6 +17,45 @@ from pykube.objects import (Job, Pod)
 ## OpenAPI generated Code stuff
 from openapi_server import util
 
+
+
+import tempfile
+import os.path
+import random
+import string
+
+from pathlib import Path
+
+def mkdtempalpha(dir=None, prefix='') -> str:
+    """Create a temporary directory name compatible with K8S pod naming conventions
+
+    :param dir
+    :type dir: str
+
+    :param prefix
+    :type prefix: str
+
+    :rtype: str
+    """
+
+    if dir is None:
+        dir = tempfile.gettempdir()
+
+    while True:
+        try:
+            random_string = prefix \
+                + ''.join(random.choices(string.ascii_lowercase, k=1)) \
+                + ''.join(random.choices(string.ascii_lowercase + string.digits, k=6)) \
+                + ''.join(random.choices(string.ascii_lowercase, k=1))
+            dirname = os.path.join(tempfile.gettempdir(), random_string)
+            Path(dirname).mkdir(parents=False, exist_ok=False)
+        except FileExistsError as e:
+            print(dirname + " already exists")
+            continue
+        break
+
+    return dirname
+
 def create_k8s_job_obj(k8s_job_name=None, dockerimage=None, cmd=None):
     """Create a job definition
 
@@ -88,16 +127,16 @@ def convert_pwi_zmz_xml2mz_ml_impl(body=None, runner=None):  # noqa: E501
     dockerimage = "chambm/pwiz-skyline-i-agree-to-the-vendor-licenses:latest"
 
     if runner == "local":
-        directory_name = tempfile.mkdtemp(prefix="msconvert-")
+        directory_name = mkdtempalpha(prefix="msconvert-")
         runcmd = ["msconvert"]
     elif runner == "docker":
-        directory_name = tempfile.mkdtemp(prefix="msconvert-")
+        directory_name = mkdtempalpha(prefix="msconvert-")
         runcmd = [ "docker", "run", "--rm", \
                    "-v", directory_name+":"+directory_name, \
                    dockerimage, \
                    "wine", "msconvert"]
     elif runner == "kubernetes":
-        directory_name = tempfile.mkdtemp(dir="/data/", prefix="msconvert-")
+        directory_name = mkdtempalpha(dir="/data/", prefix="msconvert-")
         runcmd = ["wine", "msconvert"]
     else:
         print ("unknown runner")
